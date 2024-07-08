17fe5c8712b0bffc3860883fdb19c6ab7f556ad1
Jorin Eggers <jorineggers@gmail.com>
2024-07-08 11:15:14 +0200

    new model

--- wasm_to_base64.py
+++ wasm_to_base64.py
@@ -1,7 +1,7 @@
 import base64
 
-with open('edge-impulse-standalone.wasm', 'rb') as wasm_file:
-    wasm_base64 = base64.b64encode(wasm_file.read()).decode('utf-8')
+with open("edge-impulse-standalone.wasm", "rb") as wasm_file:
+    wasm_base64 = base64.b64encode(wasm_file.read()).decode("utf-8")
 
-with open('wasm_base64.txt', 'w') as out_file:
+with open("wasm_base64.txt", "w") as out_file:
     out_file.write(wasm_base64)
